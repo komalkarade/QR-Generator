d6ed2cf1f3541cbf9bf838f7433116ef952dc48b
komal karade <123177502+komalkarade@users.noreply.github.com>
2024-04-04 15:17:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-let imgBox=document.getElementById("imgBox");
-let qrImage=document.getElementById("qrImage");
-let qrText=document.getElementById("qrText");
+const inputqr = document.getElementById("qr-input");
+const buttonqr = document.getElementById("qr-button");
+const imageqr = document.getElementById("qr-img");
+console.log(inputqr, buttonqr, imageqr);
 
-function generateQR(){
-    if(qrText.value.length>0){
-qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value;
-imgBox.classList.add("show-img");
-    }
-    else{
-        qrText.classList.add('error');
-        setTimeout(()=>{
-            qrText.classList.remove('error');
+buttonqr.addEventListener('click', () => {
 
-        },1000);
+    const inputvalue = inputqr.value;
+    console.log(inputvalue);
+
+    if (!inputvalue) {
+        alert('please enter the vaild url');
+        return;
+
     }
-}
+    else {
+
+        imageqr.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputvalue}`;
+        imageqr.alt = `QR code for ${inputvalue}`;
+    }
+
+});
